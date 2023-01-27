5e50aa1747a7a7909d5b734a9416714b3ba6c7ec
Anderson Marlon <anderson18.marlon@gmail.com>
2023-01-27 13:26:05 -0300

    Perfil Github

---
+++ blog/2023-01-27-perfil-github.md
@@ -0,0 +1,74 @@
+---
+slug: montando-um-perfil-interessante-e-exemplar-no-github
+title: Montando um perfil interessante e exemplar no GitHub
+description: Por esses dias, vejo muitos desenvolvedores júniors ou até mesmo alguém em busca de um estágio que se quer possuí um perfil no Github ou nem está atualizado, bem estruturado ou com um bom README.
+authors:
+    name: Anderson Marlon
+    title: Software Developer na Stone Co.
+    url: https://github.com/yagasaki7k
+    image_url: https://github.com/yagasaki7k.png
+---
+
+![](https://images.unsplash.com/photo-1616499370260-485b3e5ed653?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80 "Firmbee.com")
+
+> _"Muitas pessoas possuem o Github, mas não entendem o real peso, a real importância de manter seu portfólio, sua porta de entrada bem atualizada e chamativa para qualquer um olhar e se impressionar com o que você desenvolve."_
+
+Por esses dias, vejo muitos desenvolvedores júniors ou até mesmo alguém em busca de um estágio que se quer possuí um perfil no Github ou nem está atualizado, bem estruturado ou com um bom README. E isso é um grande problema, pois o Github é uma das plataformas mais utilizadas por empresas para analisar o perfil de um candidato, e se você não possui um perfil bem estruturado, você pode perder uma imensa oportunidade de emprego.
+
+Usando como base o meu próprio perfil, vou ensinar algumas técnicas que utilizo e que me auxiliarem a chegar aonde cheguei, claro, tem muita coisa que pode estar faltando e é importante, não sou uma verdade absoluta, mas é um começo.
+
+Antes de começarmos a configurar, qual é o seu foco? Atingir públicos nacionais ou internacionais?
+
+Se o foco for apenas o pessoal do Brasil, deixei em português, mas se o foco for internacional, deixe tudo em inglês, literalmente TUDO, nomes de repositórios, descrições dos projetos, os READMEs, o subtítulo de seu perfil, TUDO. Pois é uma forma de você se comunicar com o mundo, e isso é muito importante.
+
+Faça um ou o outro, nunca os dois.
+
+## Por onde começar?
+
+Vamos começar com o lado esquerdo do nosso Github
+
+<p align="center"><img src="https://user-images.githubusercontent.com/23272064/215121507-571c8c33-8f2f-4f0a-8bd1-227474d4b0a8.png" alt="Github/Yagasaki7K"/></p>
+
+O que é ideal? Novamente lembrando, não sou o dono da verdade, são apenas recomendações que sigo para que meu perfil seja bem visualizado.
+
+Se estou em busca de um emprego, o ideal é ter uma ótima foto de perfil mostrando seu profissionalismo ou seu carisma, uma foto de família, uma foto que não mostre quem de fato você é, acaba que não ajudando a definir se DE FATO, o perfil é seu. Depois que já estiver empregado, você pode colocar uma foto de família, de viagem, de anime, não tem problema algum, é mais para transmitir conforto em quem vê seu perfil e enxerga a humanidade em você.
+
+Vale lembrar que muitos - mas não todos - recrutadores possuem a mentalidade de que se você não tem uma foto de perfil, você não é uma pessoa confiável ou que possa estar se passando por outra pessoa, então, se você não tem uma foto de perfil, coloque uma foto sua, mesmo que não seja a melhor, mas que mostre quem você é.
+
+E depois, coloque o que está buscando, o que você faz ou o seu real foco, alguns exemplos a seguir são: "Desenvolvedor Front-end / React, Next, Vue" ou "DBA com MongoDB e MySQL". Isso ajuda na busca, na seguimentação do que você está procurando, qual é o seu verdadeiro foco.
+
+## O que colocar no seu README?
+
+<p align="center"><img src="https://user-images.githubusercontent.com/23272064/215122919-c585f9f7-fb75-4cce-a0ab-d12c531651e3.png" alt="Github/Yagasaki7K"/></p>
+
+Inicialmente como já definimos a nossa linguagem de comunicação (Inglês ou Português), vamos nos apresentar. Opcionalmente, ou seja, não é obrigatório, você pode colocar um banner como o cabeçalho (1500x500), nele você pode falar sobre algo que gosta na tecnologia ou até mesmo se apresentar como um .gif, informando coisas que gosta, se está procurando uma oportunidade ou não, mas não é obrigatório, é apenas uma forma de chamar a atenção de quem está vendo seu perfil.
+
+Logo abaixo, você se apresenta, como "Bem-vindo ao meu perfil", "Oi, meu nome é Fulano", ou um título seu README, enfim, é uma forma de você se apresentar para quem está vendo seu perfil de uma maneira simpática.
+
+Depois, você pode colocar um resumo sobre você, os setores de empresas em que já trabalhou para mostrar que em questão empresarial, você não possuí um nicho específico ou fala sobre você de uma forma bem breve, mas intuitiva.
+
+<p align="center"><img src="https://user-images.githubusercontent.com/23272064/215125149-c2a9139d-b0ad-4151-87ac-86302ed0c4ca.png" alt="Github/Yagasaki7K"/></p>
+
+Depois disso, fale sobre você, fale sobre alguns projetos que você carrega no peito, mas apenas indicativo, nada de fazer um mural sobre isso, senão teremos muita informações. Separe as tecnologias que você sabe, as tecnologias que está aprendendo, as tecnologias que já trabalhou e as tecnologias que pretende aprender, assim a pessoa tem uma noção maior do nível da carreira que está seguindo, os passos que está tomando e as diretivas que está seguindo. Informe também sobre o que está buscando, qual é o seu foco, se está em busca de uma mentoria, deixe explicito suas intenções.
+
+Não acho interessante informar sobre coisas que gosta de fazer como ler, jogar, ouvir música, pois isso é algo pessoal, e não é algo que vai te ajudar a ser contratado de alguma maneira, até hoje não vi como um ponto positivo durante uma entrevista. Mas se mesmo assim quiser colocar, use essa seção.
+
+E depois informe algumas curiosidades sobre você, mas de novo, focado no âmbito da tecnologia, projetos a parte que já fez, algo que já desenvolveu fora da trilha que está seguindo e seria interessante, cursos que já fez, palestras que já participou, enfim, algo que te faça ser lembrado de uma forma positiva.
+
+Você pode inserir links de publicações, como eu fiz a respeito de artigos que já escreveu, ou artigos que gostam e leva como inspiração, não é obrigatório, mas é uma forma de você mostrar que você é uma pessoa que está sempre em busca de conhecimento e que está sempre se atualizando.
+
+## Coisas que não recomendo
+
+Colocar muitas informações e transformar o seu README como um currículo não é ideal, já que existem muitas informações que não são relevantes para o recrutador, como por exemplo, colocar informações sobre o seu estado civil, se você tem filhos, se você tem animais de estimação, se você gosta de ler, se você gosta de jogar, enfim, essas informações não são relevantes para o recrutador, então, recomendo não colocar.
+
+Encher de figurinhas, como se estivéssemos na época do Orkut só mostra o quanto desorganizado visualmente é você, deixar aquela cobrinha comendo commits não é nenhum pouco interessante? O motivo? Você consome um bloco de tamanho para reservar para algo nada visualmente atrativo e que não traz nenhuma informação relevante para o recrutador.
+
+--
+
+Existem várias maneiras de se apresentar, não existe um método único, mas o recomendado é você ver como seria a visão do recrutador, a informação é relevante? Falar que você é Design Gráfico é interessante quando a vaga é de fullstack ou é interessante para uma comunidade de desenvolvimento?
+
+Coloque as suas decisões na ponta do lápis e vai filtrando com as informações que você possua.
+
+Não esqueça de deixar bem documentado seus projetos e claro, informe um link para que o recrutador possa ver o que você desenvolveu, ele não vai saber dar um git clone instalar o yarn para rodar a aplicação por exemplo. Facilite a vida deles e de quem está olhando o seu perfil.
+
+No mais é isso, qualquer dúvida, você poderá me encontrar na [Kalify Community™](https://discord.gg/jhSepmE7nN) pelo Discord.
--- src/pages/about.js
+++ src/pages/about.js
@@ -46,7 +46,10 @@ export default function About() {
             <div className="navigation">
                 <div className="navigation_left">
                     <Link href="/">
-                        <img src="https://github.com/Yagasaki7K.png" alt="Yagasaki7K" />
+                        <img
+                            src="https://github.com/Yagasaki7K.png"
+                            alt="Yagasaki7K"
+                        />
                         <h2>Yagasaki/Dev</h2>
                     </Link>
                 </div>
@@ -67,40 +70,102 @@ export default function About() {
 
             <div className="content-about">
                 <h2>I'm Yagasaki,</h2>
-                <h2>Frontend / Full Stack Software Developer (Typescript, React.js, Node.js) at Stone Co. from Campinas, Brazil</h2>
+                <h2>
+                    Frontend / Full Stack Software Developer (Typescript,
+                    React.js, Node.js) at Stone Co. from Campinas, Brazil
+                </h2>
                 <div className="quote">
-                    <p><b>❝</b> <u>Science</u> without results is just <u>witchcraft</u><b>❞</b> — Cave Johnson</p>
+                    <p>
+                        <b>❝</b> <u>Science</u> without results is just{" "}
+                        <u>witchcraft</u>
+                        <b>❞</b> — Cave Johnson
+                    </p>
                 </div>
 
-                <p>I am a <strong>code enthusiast</strong>, specialized for <strong>web</strong>. I'm Focused on modern technologies using Javascript and building great products and solutions alone or together with Kalify Inc. Persistent, dedicated and reliable worker, personally very communicative and friendly.</p>
+                <p>
+                    I am a <strong>code enthusiast</strong>, specialized for{" "}
+                    <strong>web</strong>. I'm Focused on modern technologies
+                    using Javascript and building great products and solutions
+                    alone or together with Kalify Inc. Persistent, dedicated and
+                    reliable worker, personally very communicative and friendly.
+                </p>
 
                 <h3>Skills and expertise:</h3>
 
                 <div className="languages">
-                    <p><strong>Languages:</strong> Javascript (Typescript)</p>
-                    <p><strong>Frontend:</strong> React.js, Next.js</p>
-                    <p><strong>Backend:</strong> Node.js, Express, Firebase</p>
-                    <p><strong>Database:</strong> Firebase</p>
-                    <p><strong>Testing:</strong> Jest</p>
-                    <p><strong>DevOps:</strong> Docker, CI/CD</p>
-                    <p><strong>Design:</strong> Figma, UX/UI</p>
-                    <p><strong>Other:</strong> Git, Serverless, Vercel, etc...</p>
+                    <p>
+                        <strong>Languages:</strong> Javascript (Typescript)
+                    </p>
+                    <p>
+                        <strong>Frontend:</strong> React.js, Next.js
+                    </p>
+                    <p>
+                        <strong>Backend:</strong> Node.js, Express, Firebase
+                    </p>
+                    <p>
+                        <strong>Database:</strong> Firebase
+                    </p>
+                    <p>
+                        <strong>Testing:</strong> Jest
+                    </p>
+                    <p>
+                        <strong>DevOps:</strong> Docker, CI/CD
+                    </p>
+                    <p>
+                        <strong>Design:</strong> Figma, UX/UI
+                    </p>
+                    <p>
+                        <strong>Other:</strong> Git, Serverless, Vercel, etc...
+                    </p>
                 </div>
 
                 <h3>Positions:</h3>
 
                 <div className="positions">
                     <h4>Software Developer</h4>
-                    <p><strong>Linx/Stone Co.</strong> | <i>October 2022 — Currently</i></p>
-                    <p>Working in A.I. and e-commerce applications. Using: React, NextJS, TypeScript, NodeJS, GraphQL, Hasura, UI/UX, Storybook, Figma, ChakraUI, CI/CD, Unit and Integration Tests, AWS Cloud and Postgress</p>
+                    <p>
+                        <strong>Linx/Stone Co.</strong> |{" "}
+                        <i>October 2022 — Currently</i>
+                    </p>
+                    <p>
+                        Working in A.I. and e-commerce applications. Using:
+                        React, NextJS, TypeScript, NodeJS, GraphQL, Hasura,
+                        UI/UX, Storybook, Figma, ChakraUI, CI/CD, Unit and
+                        Integration Tests, AWS Cloud and Postgress
+                    </p>
 
                     <h4>Systems Development Analyst</h4>
-                    <p><strong>Cenário Capital</strong> | <i>April 2021 — September 2021</i></p>
-                    <p>Cenário Capital is a Brazilian company Founded in 2008 with expertise in digital marketing and analytical focus on different types of digital business. We're using technologies like NextJS, Typescript, Push Notification using Javascript via Google Developers, Scrum and Kabam during development, Google Analytics, Facebook Ads and using Digital Ocean to DevOps our system. <br /><br />Project available at <Link href="https://credits.com.br/">https://credits.com.br/</Link></p>
+                    <p>
+                        <strong>Cenário Capital</strong> |{" "}
+                        <i>April 2021 — September 2021</i>
+                    </p>
+                    <p>
+                        Cenário Capital is a Brazilian company Founded in 2008
+                        with expertise in digital marketing and analytical focus
+                        on different types of digital business. We're using
+                        technologies like NextJS, Typescript, Push Notification
+                        using Javascript via Google Developers, Scrum and Kabam
+                        during development, Google Analytics, Facebook Ads and
+                        using Digital Ocean to DevOps our system. <br />
+                        <br />
+                        Project available at{" "}
+                        <Link href="https://credits.com.br/">
+                            https://credits.com.br/
+                        </Link>
+                    </p>
 
                     <h4>Junior Marketing Assistant / Development Frontend</h4>
-                    <p><strong>Marry Perry Paris</strong> | <i>July 2020 – October 2020</i></p>
-                    <p>I was responsible for the development of several landing pages for the company, including the main website, how to use each product website and several other segments for customer communication using MVC Architecture, MySQL and NodeJS – later the project was changed to Laravel</p>
+                    <p>
+                        <strong>Marry Perry Paris</strong> |{" "}
+                        <i>July 2020 – October 2020</i>
+                    </p>
+                    <p>
+                        I was responsible for the development of several landing
+                        pages for the company, including the main website, how
+                        to use each product website and several other segments
+                        for customer communication using MVC Architecture, MySQL
+                        and NodeJS – later the project was changed to Laravel
+                    </p>
                 </div>
 
                 <h3>Certificates:</h3>
@@ -109,7 +174,7 @@ export default function About() {
                     <p>
                         <strong>
                             Modern Web with Javascript + Projects
-                            (JS/Typescript/React/Node/ESNext){" "}
+                            (JS/Typescript/React/Node){" "}
                         </strong>
                         by COD3R - August 2022
                     </p>
@@ -217,21 +282,26 @@ export default function About() {
                         by MacPoli Cursos - December 2014
                     </p>
                 </div>
-
-
             </div>
 
             <div className="footer">
                 <div className="footer_left">
                     <strong>Copyright</strong>
-                    <p>© {new Date().getFullYear()} <Link href="/">Anderson "Yagasaki" Marlon</Link></p>
+                    <p>
+                        © {new Date().getFullYear()}{" "}
+                        <Link href="/">Anderson "Yagasaki" Marlon</Link>
+                    </p>
                 </div>
                 <div className="footer_center">
                     <strong>Contact</strong>
                     <Link href="mailto:anderson18.marlon@gmail.com">Email</Link>
-                    <Link href="https://linkedin.com/in/andersonmarlon">Linkedin</Link>
+                    <Link href="https://linkedin.com/in/andersonmarlon">
+                        Linkedin
+                    </Link>
                     <Link href="https://twitter.com/Yagasaki7K">Twitter</Link>
-                    <Link href="https://api.whatsapp.com/send?phone=5519998018174">Whatsapp</Link>
+                    <Link href="https://api.whatsapp.com/send?phone=5519998018174">
+                        Whatsapp
+                    </Link>
                 </div>
                 <div className="footer_right">
                     <strong>Check my work</strong>
