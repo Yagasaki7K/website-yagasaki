96a812397ce0627ce841e3ac50dc7f521cf02341
Anderson Marlon <anderson18.marlon@gmail.com>
2023-03-24 16:45:33 -0300

    Post PWA + Update Docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,9 +31,18 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          // sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -44,6 +53,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "https://i.imgur.com/1ypU0Xs.png",
       navbar: {
         title: "Yagasaki/Blog",
         logo: {
@@ -131,7 +141,6 @@ const config = {
         defaultMode: "dark",
         disableSwitch: true,
       },
-      image: "https://i.imgur.com/1ypU0Xs.png",
     }),
 };
 
