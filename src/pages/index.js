019efb95861d111a28e9ef7af1d4e2c0bb0c0a30
Anderson Marlon <anderson18.marlon@gmail.com>
2023-11-02 00:27:30 -0300

    Fix: SEO Thumbnail

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,17 +29,31 @@ export default function Home() {
                     async
                     src="https://www.googletagmanager.com/gtag/js?id=G-39P50954DX"
                 />
-                <meta name="twitter:card" content={"summary_large_image"} />
-                <meta name="twitter:site" content={"@Yagasaki7K"} />
-                <meta name="twitter:title" content={"Anderson Marlon // Yagasaki"} />
+
+                <meta name="description" content="Software Developer & Front-end Specialist" />
                 <meta
-                    name="twitter:description"
-                    content={"Software Developer @ Linx"}
+                    name="keywords"
+                    content="Desenvolvedor front-end, Front-end developer, Desenvolvedor web, Web developer, Desenvolvedor de interfaces, Interface developer, Desenvolvedor de UX, UX developer, Desenvolvedor de UI, UI developer, Software developer, Programador, Developer, Engenheiro de software, Software engineer, Desenvolvedor de back-end, Back-end developer, Desenvolvedor full-stack, Full-stack developer, Desenvolvedor de jogos, Game developer, Agência, Agência de desenvolvimento, Web agency, Software agency, Agência de marketing digital, Digital marketing agency, Agência de design, Design agency, Freelance, Desenvolvedor freelancer, Freelance developer, Programador freelancer, Freelance programmer, Engenheiro de software freelancer, Freelance software engineer, react, nextjs, next, typescript, styled, styled components, components, firebase, rocketchat, docker"
                 />
+
+                <meta property="og:title" content="Anderson Marlon // Yagasaki" />
+                <meta property="og:description" content="Software Developer & Front-end Specialist" />
                 <meta
-                    name="twitter:image"
-                    content={"https://i.imgur.com/1ypU0Xs.png"}
+                    property="og:keywords"
+                    content="Desenvolvedor front-end, Front-end developer, Desenvolvedor web, Web developer, Desenvolvedor de interfaces, Interface developer, Desenvolvedor de UX, UX developer, Desenvolvedor de UI, UI developer, Software developer, Programador, Developer, Engenheiro de software, Software engineer, Desenvolvedor de back-end, Back-end developer, Desenvolvedor full-stack, Full-stack developer, Desenvolvedor de jogos, Game developer, Agência, Agência de desenvolvimento, Web agency, Software agency, Agência de marketing digital, Digital marketing agency, Agência de design, Design agency, Freelance, Desenvolvedor freelancer, Freelance developer, Programador freelancer, Freelance programmer, Engenheiro de software freelancer, Freelance software engineer, react, nextjs, next, typescript, styled, styled components, components, firebase, rocketchat, docker"
                 />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:image"
+                    content="https://cdn-images-1.medium.com/max/800/0*eBBvNAU5Qz9roeNf.png"
+                />
+                <meta property="og:site_name" content="Anderson Marlon // Yagasaki" />
+                <meta property="og:locale" content="en_US" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Anderson Marlon // Yagasaki" />
+                <meta name="twitter:description" content="Software Developer & Front-end Specialist" />
+                <meta name="twitter:image:alt" content="Anderson Marlon // Yagasaki" />
             </Head>
 
             <div className="homePage">
@@ -48,7 +62,7 @@ export default function Home() {
                 <div className="content">
                     <h3><img src="https://github.com/tairosonloa/tairosonloa/raw/main/assets/wave.gif?raw=true" width="25px" /> Hi, I'm Anderson Marlon</h3>
                     <h2>Software <br /> Developer</h2>
-                    <h4>ReactJS | NextJS | Typescript | Styled Components | Firebase | Docker</h4>
+                    <h4>ReactJS | NextJS | Typescript | Styled Components | Firebase | Docker | Rocketchat</h4>
                 </div>
 
                 <Footer />
