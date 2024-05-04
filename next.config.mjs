655685ff1203c31f46a234a29bd632703cd3a51f
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-03 23:29:27 -0300

    Added: o-que-e-lazy-loading - 03-05 23:29

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import million from 'million/compiler';
 
 const nextConfig = {
   reactStrictMode: true,
@@ -53,4 +52,4 @@ const millionConfig = {
   }
 }
 
-export default million.next(nextConfig, millionConfig);
+export default nextConfig;
