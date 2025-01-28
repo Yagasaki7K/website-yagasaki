7b60ca44f80f57df14b1dfb5511f4bd514600247
Anderson Marlon <anderson18.marlon@gmail.com>
2025-01-28 12:44:57 -0300

    Added: hugging-face-facilita-para-desenvolvedores-rodarem-modelos-de-ia-em-nuvens-de-terceiros.mdx - 28-01 12:44

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'techcrunch.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'w.wallhaven.cc',
       },
       {
