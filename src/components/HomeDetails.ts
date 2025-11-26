1fcbc484f09600744f7caa3a48cfafff02f1cd30
Anderson Marlon <yagasakiwanderlust@proton.me>
2025-11-26 17:32:35 -0300

    Added: New Certificate + Saas Page - 26-11 17:32

--- src/components/HomeDetails.ts
+++ src/components/HomeDetails.ts
@@ -56,6 +56,61 @@ const HomeDetails = styled.div`
         }
     }
 
+    .hero-saas {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: left;
+        margin: 1rem auto;
+        max-width: 55rem;
+
+        h1 {
+            font-size: 36px;
+            font-weight: 600;
+            color: var(--white);
+            margin-bottom: 2rem;
+        }
+
+        p {
+            margin: 1rem 0;
+            line-height: 1.8rem;
+        }
+
+        .p {
+            font-size: 1rem;
+            margin-bottom: 10px;
+            display: flex;
+
+            .link {
+                margin: 0 4px;
+                background: var(--gray-transparent);
+                border-radius: 5px;
+                font-size: 0.9rem;
+                padding: 2px 4px;
+                color: var(--gray);
+                transition: 1s all ease;
+                display: flex;
+                align-items: center;
+
+                a {
+                    text-decoration: none;
+                    color: var(--gray);
+                    transition: 1s all ease;
+
+                    &:hover {
+                        color: var(--white);
+                    }
+                }
+
+                img {
+                    width: 15px;
+                    margin-right: 5px;
+                }
+            }
+        }
+    }
+
     .blogSearch {
         display: flex;
         flex-direction: column;
