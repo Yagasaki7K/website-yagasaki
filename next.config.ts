0a2a30a8e2d7652b364f31218286b8faf70507e7
Anderson Marlon <anderson18.marlon@gmail.com>
2026-01-09 11:26:15 -0300

    Update: next.config.js to output export - 09-01 11:26

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactStrictMode: true,
-    output: "export",
     compiler: {
         styledComponents: true,
+    },
+    /* Exports Settings  */
+    output: "export",
+    images: {
+        unoptimized: true,
     },
 };
 
