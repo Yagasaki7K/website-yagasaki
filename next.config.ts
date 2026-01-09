3b91394c70a625ddb9fee812c428748528b85e4c
Anderson Marlon <anderson18.marlon@gmail.com>
2026-01-09 11:30:16 -0300

    Update: allowedDevOrigins in next.config.js - 09-01 11:30

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true,
     },
+    allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
 };
 
 export default nextConfig;
